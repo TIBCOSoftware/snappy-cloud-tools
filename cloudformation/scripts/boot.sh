d1c1ae3cae18ab26a6b99577c1f8c836a026c353
Amogh Shetkar <ashetkar@snappydata.io>
2016-09-23 18:35:21 +0530

    * Update the log statements with timestamp.

--- cloudformation/scripts/boot.sh
+++ cloudformation/scripts/boot.sh
@@ -11,18 +11,19 @@ apt-get update
 apt-get -y upgrade
 apt-get -y install python-setuptools  
 easy_install https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz
+printf "# `date` Installed cloudformation helper scripts $?\n" >> status.log
 
 # Update the urls for various artifacts and scripts.
 printf "https://github.com/SnappyDataInc/snappydata/releases/download/v0.6/snappydata-0.6-bin.tar.gz" > snappydata-url.txt
 printf "https://github.com/SnappyDataInc/zeppelin-interpreter/releases/download/v0.6/snappydata-zeppelin-0.6.jar" > interpreter-url.txt
 printf "https://github.com/SnappyDataInc/zeppelin-interpreter/raw/notes/examples/notebook/notebook.tar.gz" > notebook-url.txt
 printf "https://github.com/SnappyDataInc/aws-cloud/raw/master/cloudformation/scripts/setup.sh" > cf-script-url.txt
-printf "Updated urls $?\n" >> status.log
+printf "# `date` Updated urls $?\n" >> status.log
 
 rm setup.sh
 CF_SCRIPT_URL=`cat cf-script-url.txt`
 wget ${CF_SCRIPT_URL}
-printf "Downloaded cloudformation script $?\n" >> status.log
+printf "# `date` Downloaded cloudformation script $?\n" >> status.log
 
 bash setup.sh
 SETUP_DONE=`echo $?`
