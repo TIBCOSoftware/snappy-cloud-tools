3b1b884d2155b0d301a683fda12509c694a61e8b
Amogh Shetkar <ashetkar@snappydata.io>
2016-12-03 18:50:03 +0530

    * Scripts for quickstart notebook. * These may be merged into one later.

---
+++ cloudformation/scripts/quickstart/boot.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -x
+# Run as root
+
+printf "\n# `date` START -------------------------------------------------- #\n" >> status.log
+
+mkdir -p /snappydata/downloads
+cd /snappydata/downloads
+
+# Install CloudFormation helper scripts
+which cfn-signal
+if [[ $? -ne 0 ]]; then
+  apt-get update
+  apt-get -y upgrade
+  apt-get -y install python-setuptools
+  easy_install https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz
+  printf "# `date` Installed cloudformation helper scripts $?\n" >> status.log
+fi
+
+# Update the urls for various artifacts and scripts.
+printf "https://github.com/SnappyDataInc/snappydata/releases/download/0.7/snappydata-0.7-bin.tar.gz" > snappydata-url.txt
+printf "https://github.com/SnappyDataInc/zeppelin-interpreter/releases/download/0.7/snappydata-zeppelin-0.7.jar" > interpreter-url.txt
+printf "https://s3-us-west-2.amazonaws.com/zeppelindemo/quickstart/notebook.tar.gz" > notebook-url.txt
+printf "https://github.com/SnappyDataInc/aws-cloud/raw/master/cloudformation/scripts/quickstart/setup.sh" > cf-script-url.txt
+printf "# `date` Updated urls $?\n" >> status.log
+
+rm setup.sh
+CF_SCRIPT_URL=`cat cf-script-url.txt`
+wget ${CF_SCRIPT_URL}
+printf "# `date` Downloaded cloudformation setup script $?\n" >> status.log
+
+bash setup.sh
+SETUP_DONE=`echo $?`
+
+printf "\n# `date` END -------------------------------------------------- #\n" >> status.log
+
+exit ${SETUP_DONE}
